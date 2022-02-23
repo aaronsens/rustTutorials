bdfa4c0e644b2cacef9bb44ac4b804abb0e0e936
aaronsens <36092424+aaronsens@users.noreply.github.com>
2022-02-23 14:22:42 -0500

    first commit

---
+++ variables/src/main.rs
@@ -0,0 +1,22 @@
+// fn main() {
+//     let mut x = 5;
+//     println!("The value of x is: {}", x);
+//     x = 6;
+//     println!("The value of x is: {}", x);
+// }
+
+fn main() {
+    let x = 5;
+
+    let x = x + 1;
+
+    //by using let, we can perform a few transformations on a value but have the variable be 
+    //immutable after those transformations have been completed
+
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {}", x);
+    }
+
+    println!("The value of x is: {}", x);
+}
